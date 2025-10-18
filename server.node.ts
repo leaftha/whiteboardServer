c577bd176658a3c16226481937bbaf30e4affced
SaChan <vltpcks@gmail.com>
2025-10-18 23:51:04 +0900

    Update server.node.ts

--- server.node.ts
+++ server.node.ts
@@ -15,7 +15,7 @@ const LIVEKIT_API_SECRET = process.env.LIVEKIT_API_SECRET ?? "";
 const app = fastify();
 
 app.register(cors, {
-  origin: "http://localhost:3000",
+  origin: "https://whiteboard-sepia-five.vercel.app/",
   methods: ["GET", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
 });
